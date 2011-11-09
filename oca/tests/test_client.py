c2631910b5fe327c2581a00b63193351f474b04e
Łukasz Oleś <lukaszoles@gmail.com>
2011-11-09 10:22:27 +0100

    Update CLient tests.

--- oca/tests/test_client.py
+++ oca/tests/test_client.py
@@ -1,80 +1,93 @@
 # -*- coding: UTF-8 -*-
+import os
+import socket
+
 from mock import Mock
 from nose.tools import raises
 
-from oca import Client, os, OpenNebulaException, socket
+import oca
 
 
 class TestClient:
     def test_secret(self):
-        c = Client('test:test')
+        c = oca.Client('test:test')
         assert c.one_auth == 'test:a94a8fe5ccb19ba61c4c0873d391e987982fbbd3'
 
     def test_one_auth(self):
-        os.environ["ONE_AUTH"] = 'fixtures/one_auth'
+        os.environ["ONE_AUTH"] = os.path.join(os.path.dirname(oca.__file__),
+                                             'tests/fixtures/one_auth')
         try:
-            c = Client()
+            c = oca.Client()
             secret = 'test:a94a8fe5ccb19ba61c4c0873d391e987982fbbd3'
             assert c.one_auth == secret
         finally:
             os.environ["ONE_AUTH"] = ''
 
     def test_default_user_path(self):
-        Client.DEFAULT_ONE_AUTH = 'fixtures/one_auth'
-        c = Client()
+        os.environ["ONE_AUTH"] = os.path.join(os.path.dirname(oca.__file__),
+                                             'tests/fixtures/one_auth')
+        c = oca.Client()
         assert c.one_auth == 'test:a94a8fe5ccb19ba61c4c0873d391e987982fbbd3'
 
-    @raises(OpenNebulaException)
+    @raises(oca.OpenNebulaException)
     def test_wrong_default_user_path(self):
-        Client.DEFAULT_ONE_AUTH = '/ad/ads/a/das/d/sad/sad/sa/d/one_auth'
-        c = Client()
+        oca.Client.DEFAULT_ONE_AUTH = '/ad/ads/a/das/d/sad/sad/sa/d/one_auth'
+        c = oca.Client()
 
-    @raises(OpenNebulaException)
+    @raises(oca.OpenNebulaException)
     def test_invalid_secret(self):
-        c = Client('testtest')
+        c = oca.Client('testtest')
 
     def test_with_plain(self):
-        c = Client('test:plain:a94a8fe5ccb19ba61c4c0873d391e987982fbbd3')
+        c = oca.Client('test:plain:a94a8fe5ccb19ba61c4c0873d391e987982fbbd3')
         assert c.one_auth == 'test:a94a8fe5ccb19ba61c4c0873d391e987982fbbd3'
 
     def test_addres(self):
-        c = Client('test:test',  "http://8.8.8.8:2633/RPC2")
+        c = oca.Client('test:test',  "http://8.8.8.8:2633/RPC2")
         assert c.one_address == "http://8.8.8.8:2633/RPC2"
 
     def test_one_xml_rpc(self):
         os.environ["ONE_XMLRPC"] = "http://8.8.8.8:2633/RPC2"
         try:
-            c = Client()
+            c = oca.Client()
             assert c.one_address == "http://8.8.8.8:2633/RPC2"
         finally:
             os.environ["ONE_XMLRPC"] = ''
 
     def test_defaul_xmlrpc(self):
-        c = Client('test:test')
-        assert c.one_address == Client.DEFAULT_ONE_ADDRESS
+        c = oca.Client('test:test')
+        assert c.one_address == oca.Client.DEFAULT_ONE_ADDRESS
 
     def test_return_two_values_call(self):
-        c = Client('test:test')
+        c = oca.Client('test:test')
         c.server.one = Mock()
-        c.server.one.test_method = lambda x: [1, '2']
+        c.server.one.test_method = lambda x: [True, '2', 0]
         assert c.call('test_method') == '2'
 
     def test_return_one_value_call(self):
-        c = Client('test:test')
+        c = oca.Client('test:test')
         c.server.one = Mock()
-        c.server.one.test_method = lambda x: [1]
+        c.server.one.test_method = lambda x: [True, '', 0]
         assert c.call('test_method') == ''
 
-    @raises(OpenNebulaException)
+    @raises(oca.OpenNebulaException)
     def test_retrurn_error_code_0_call(self):
-        c = Client('test:test')
+        c = oca.Client('test:test')
         c.server.one = Mock()
-        c.server.one.test_method = lambda x: [0, '2']
+        c.server.one.test_method = lambda x: [False, '2', 1]
+        c.call('test_method')
+
+
+    @raises(oca.OpenNebulaException)
+    def test_invalid_call(self):
+        c = oca.Client('test:test')
+        c.server.one = Mock()
+        c.server.one.test_method = lambda x: [False, '2']
         c.call('test_method')
 
     @raises(socket.error)
     def test_connection_error(self):
-        c = Client('test:test')
+        c = oca.Client('test:test')
         c.server.one = Mock()
         c.server.one.test_method = Mock(side_effect=socket.error(1))
         c.call('test_method')
