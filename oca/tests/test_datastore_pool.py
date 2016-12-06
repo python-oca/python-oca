c28c49ddc6fe2f7039cd7b350772d841e16235c7
Mariano Guezuraga <mariano.guezuraga@booking.com>
2016-12-06 13:45:30 +0100

    Add missing datastore pool info test. Fixture was already present

---
+++ oca/tests/test_datastore_pool.py
@@ -0,0 +1,21 @@
+# -*- coding: UTF-8 -*-
+import os
+import unittest
+
+from mock import Mock
+
+import oca
+
+
+class TestDatastorePool(unittest.TestCase):
+    def setUp(self):
+        self.client = oca.Client('test:test')
+        self.xml = open(os.path.join(os.path.dirname(oca.__file__),
+                                     'tests/fixtures/datastorepool.xml')).read()
+
+    def test_info(self):
+        self.client.call = Mock(return_value=self.xml)
+        pool = oca.DatastorePool(self.client)
+        pool.info()
+        assert len(list(pool)) == 2
+
