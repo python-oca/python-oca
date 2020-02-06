c66104ea82d966eea1c0b48b4f42b3a92ff8991a
Riccardo Murri <riccardo.murri@gmail.com>
2020-02-06 11:07:39 +0100

    Fix `coding:` line to be compatible with Emacs.
    Emacs complains that `UTF-8` (all uppercase) is not a valid name of a
    coding system:
    
        Warning (mule): Invalid coding system ‘UTF-8’ is specified
        for the current buffer/file by the :coding tag.
        It is highly recommended to fix it before writing to a file.
    
    Using the lowercase version solves the problem.

--- oca/__init__.py
+++ oca/__init__.py
@@ -1,4 +1,4 @@
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
 import http.client
 import os
 import re
--- oca/pool.py
+++ oca/pool.py
@@ -1,4 +1,4 @@
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
 import xml.etree.ElementTree as ET
 
 from .exceptions import OpenNebulaException
